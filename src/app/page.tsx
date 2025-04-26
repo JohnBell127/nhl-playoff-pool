b69b75074f914e42259d993bf0c268cb66ca7aa1
JohnBell127 <x2020cvm@stfx.ca>
2025-04-26 17:01:54 -0300

    vervel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Leaderboard from "../../components/Leaderboard";
 import Layout from "../../components/Layout";
 
+import { Analytics } from '@vercel/analytics/next';
+
 export default function Home() {
   return (
     <Layout>
+      <Analytics />
       <div className="container mx-auto py-8 px-4">
         <Leaderboard />
       </div>
