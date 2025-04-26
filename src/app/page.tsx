10fa14920163b153a6cb60100cc2f0df23fe3d8a
JohnBell127 <x2020cvm@stfx.ca>
2025-04-26 17:05:06 -0300

    analytics fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Leaderboard from "../../components/Leaderboard";
 import Layout from "../../components/Layout";
 
-import { Analytics } from '@vercel/analytics/next';
-
 export default function Home() {
   return (
     <Layout>
-      <Analytics />
       <div className="container mx-auto py-8 px-4">
         <Leaderboard />
       </div>
